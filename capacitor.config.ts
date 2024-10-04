da56733c58ff62c21f80c87b16c53aa56f2c7771
brenddamachado <brenda16machado.bm@gmail.com>
2024-10-04 17:29:50 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pokéApi',
+  webDir: 'www'
+};
+
+export default config;
